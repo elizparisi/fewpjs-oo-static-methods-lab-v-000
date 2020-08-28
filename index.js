06e5068b3d7a664ef5a2338c5ffee77a21ae314f
Liz P <parisielizabeth@gmail.com>
2020-08-28 03:13:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ class Formatter {
     return result.join(" ");
   }
 }
+
+    let result =[];
+    let arrayOfWords = sentence.split(" ");
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
+}
